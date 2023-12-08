6f483a9da097d0bb9464c3bd34cc95b5655c4fc3
James F West <jfwest1094@gmail.com>
2023-12-08 17:57:08 -0600

    finished 2.1 working on 2.2

---
+++ Puzzle2.2/puzzle2-2.py
@@ -0,0 +1,36 @@
+import re
+total = 0
+
+with open("Puzzle2.2/input.txt") as file:
+    while line := file.readline():
+        colors = ['eulb', 'neerg', 'der', 'emaG']
+        gameNum = re.search('[0-9]+', line).group()
+        game = re.split('[;:]+', line)
+        gameBool = True
+
+        for section in game[1:]:
+            greenNum = 0
+            blueNum = 0
+            redNum = 0
+            test = section.split(',')  # Initialize test list for each section
+
+            for part in test:
+                two = re.search('|'.join(colors), part[::-1]).group()
+
+                for color in colors:
+                    if two == color:
+                        match two:
+                            case 'neerg':
+                                greenNum += int(re.search('[0-9]+', part).group())
+                            case 'eulb':
+                                blueNum += int(re.search('[0-9]+', part).group())
+                            case 'der':
+                                redNum += int(re.search('[0-9]+', part).group())
+                if redNum > 12 or blueNum > 14 or greenNum > 13:
+                    gameBool = False
+                    break
+
+        if gameBool:
+            total += int(gameNum)
+
+    print(total)
