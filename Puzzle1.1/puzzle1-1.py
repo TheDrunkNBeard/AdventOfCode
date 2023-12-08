77d2e5af0c866ec39fd35e86421282ff3ed1ba13
James F West <jfwest1094@gmail.com>
2023-12-08 16:14:14 -0600

    changed file name

--- Puzzle1.1/puzzle1-1.py
+++ Puzzle1.1/puzzle1-1.py
@@ -1,6 +1,6 @@
 import re
 total = 0
-with open("Puzzle1/input.txt") as file:
+with open("Puzzle1.1/input.txt") as file:
     while line := file.readline():
         one = re.search('[0-9]', line).group()
         two = re.search('[0-9]', line[::-1]).group()
